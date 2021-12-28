06b5f7981e96e97e82e5be3a2390297eda965784
Viktor Kocur <kocurvik@gmail.com>
2021-12-28 20:40:39 +0100

    minor annotator changes

--- eval/annotator.py
+++ eval/annotator.py
@@ -4,7 +4,8 @@ import pickle
 import cv2
 import numpy as np
 
-from utils.image import load_lr_img_from_gwy
+from utils.image import load_lr_img_from_gwy, normalize
+
 
 def annotate_entries(entries):
     for i, entry in enumerate(entries):
@@ -20,16 +21,16 @@ def annotate_entries(entries):
                 clicked.append([x, y])
                 print("Clicked x: {}, y: {}".format(x, y))
 
-        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
         cv2.setMouseCallback('image', mouse_callback)
-        cv2.imshow('image', entry['img_l'])
+        cv2.imshow('image', normalize(entry['img_l']))
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
         entry['registration_points'] = clicked
 
         if i == 0:
-            example_img = cv2.cvtColor(entry['img_l'], cv2.COLOR_GRAY2BGR)
+            example_img = cv2.cvtColor(normalize(entry['img_l']), cv2.COLOR_GRAY2BGR)
             for pt in clicked:
                 example_img = cv2.circle(example_img, (pt[0], pt[1]), 3, (0, 0, 255), thickness=2)
 
@@ -44,7 +45,7 @@ def extract_eval_data(path):
 
     for filename in filenames:
         gwy_path = os.path.join(path, filename)
-        img_l, img_r = load_lr_img_from_gwy(gwy_path)
+        img_l, img_r = load_lr_img_from_gwy(gwy_path, normalize_range=False)
 
         entry_dict = {'filename': filename, 'gwy_path': gwy_path, 'img_l': img_l, 'img_r': img_r}
 
@@ -59,7 +60,8 @@ def save_entries(entries, path):
 
 
 if __name__ == '__main__':
-    path = 'D:/Research/data/GEFSEM/2021-04-07 - Dataset/TGZ3'
+    # path = 'D:/Research/data/GEFSEM/2021-04-07 - Dataset/Neno'
+    path = 'D:/Research/data/GEFSEM/EvalData/TGZ3'
     entries = extract_eval_data(path)
     annotate_entries(entries)
     save_entries(entries, path)
